72c7c9192a21d38b49e32d83ba5218c56f5f98e7
Allison Snipes <ajsnipes@usfca.edu>
2019-02-19 01:21:55 -0800

    button 3

--- javascript.js
+++ javascript.js
@@ -1,2 +1,17 @@
 'use strict'
 
+//when the blue button is clicked turn the box blue
+document.getElementById("button2").addEventListener("click", function() {
+  document.getElementById("box").style.backgroundColor = "Blue";}
+);
+
+//when grow button is clicked
+$("#button1").on("click", function() {
+  //box should grow
+  $("#box").animate({height:"+=70px", width:"+=70px"}, "fast");
+})
+
+//fade the box out
+#button3{
+  transition: all 2s linear;
+}
